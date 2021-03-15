ce121dd9d98a385a30594d406a8a2b59158d4a84
Luca Lenzarini <luca.lenzarini@hotmail.com>
2021-03-15 01:07:57 -0300

    add default parameters and filling of empty fields

--- csvmerger.py
+++ csvmerger.py
@@ -2,22 +2,39 @@
 import datacompy, pandas as pd
 import numpy as np
 
-def merge_csv(files_names: list, index_keys: list, merge_file_name):
+def merge_csv(files_paths: list, merge_file_name="merged", index_keys=["Data"]):
 
+    # if no index_key was passed
+    if index_keys[0] == "Data":
+        index_keys = ["Data"]*len(files_paths)
+
+    # create empty dataframe
     fullDataframe = pd.DataFrame()
 
-    for i in range(len(files_names)):
-        dataframe = pd.read_csv(files_names[i], decimal=',')
-
+    for i in range(len(files_paths)):
+        dataframe = pd.read_csv(files_paths[i], decimal=',')
         dataframe.rename(columns={index_keys[i]: 'Data'}, inplace = True)
 
+        how = 'right'
+        # check wich size has more rows
+        if len(fullDataframe) > len(dataframe):
+            how = 'left'
+
         if i != 0:
-            result = pd.merge(fullDataframe, dataframe, on='Data')
-            fullDataframe = result
+            # merge both dataframes
+            fullDataframe = pd.merge(fullDataframe, dataframe, on='Data', how=how)
         else:
+            # if it's the first dataframe, there is nothing to merge
             fullDataframe = dataframe
+
+        # fill empty columns with previous or posterior values
+        fullDataframe.fillna(method='ffill', inplace=True)
+        fullDataframe.fillna(method='bfill', inplace=True)
 
     merge_file_name = merge_file_name + '.csv'
 
     print("Successfully merged into", merge_file_name)
+
     fullDataframe.to_csv(merge_file_name, index = False, header=True)
+
+merge_csv(files_paths=['teste1.csv', 'teste2.csv'])
